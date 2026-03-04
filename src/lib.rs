ca0d188dfc2cde6cbf7acd57ab5434bd35cfc0a0
Carl Österberg <carl.vilhelms.osterberg@gmail.com>
2026-03-04 21:15:31 +0100

    inital

---
+++ src/lib.rs
@@ -0,0 +1,172 @@
+
+use std::fmt;
+use std::ops::*;
+
+pub trait Numeric:
+    Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + AddAssign
+    + Copy
+    + Default
+    + PartialEq
+    + fmt::Display
+{
+}
+
+impl<
+    T: Add<Output = T>
+        + Sub<Output = T>
+        + Mul<Output = T>
+        + AddAssign
+        + Copy
+        + Default
+        + PartialEq
+        + fmt::Display,
+> Numeric for T
+{
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Matrix<T: Numeric, const M: usize, const N: usize> {
+    array: [[T; N]; M],
+}
+
+impl<T: Numeric, const M: usize, const N: usize> Matrix<T, M, N> {
+    pub fn new() -> Matrix<T, M, N> {
+        Matrix {
+            array: [[T::default(); N]; M],
+        }
+    }
+
+    pub fn from_array(array: [[T; N]; M]) -> Matrix<T, M, N> {
+        Matrix { array }
+    }
+
+    pub fn transpose(&self) -> Matrix<T, N, M> {
+        let mut array = [[T::default(); M]; N];
+        for m in 0..M {
+            for n in 0..N {
+                array[n][m] = self.array[m][n];
+            }
+        }
+        Matrix { array }
+    }
+}
+
+impl<T: Numeric, const M: usize, const N: usize> PartialEq for Matrix<T, M, N> {
+    fn eq(&self, rhs: &Matrix<T, M, N>) -> bool {
+        self.array == rhs.array
+    }
+
+    fn ne(&self, rhs: &Matrix<T, M, N>) -> bool {
+        self.array != rhs.array
+    }
+}
+
+impl<T: Numeric, const M: usize, const N: usize> Add for Matrix<T, M, N> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        let mut array = [[T::default(); N]; M];
+        for m in 0..M {
+            for n in 0..N {
+                array[m][n] = self.array[m][n] + rhs.array[m][n];
+            }
+        }
+        Matrix { array }
+    }
+}
+
+impl<T: Numeric, const M: usize, const N: usize> Sub for Matrix<T, M, N> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        let mut array = [[T::default(); N]; M];
+        for m in 0..M {
+            for n in 0..N {
+                array[m][n] = self.array[m][n] - rhs.array[m][n];
+            }
+        }
+        Matrix { array }
+    }
+}
+
+impl<T: Numeric, const M: usize, const N: usize> Mul<T> for Matrix<T, M, N> {
+    type Output = Self;
+
+    fn mul(self, scalar: T) -> Self {
+        let mut array = [[T::default(); N]; M];
+        for m in 0..M {
+            for n in 0..N {
+                array[m][n] = self.array[m][n] * scalar;
+            }
+        }
+        Matrix { array }
+    }
+}
+
+impl<T: Numeric, const M: usize, const N: usize, const P: usize> Mul<Matrix<T, N, P>>
+    for Matrix<T, M, N>
+{
+    type Output = Matrix<T, M, P>;
+
+    fn mul(self, rhs: Matrix<T, N, P>) -> Matrix<T, M, P> {
+        let mut array = [[T::default(); P]; M];
+        for m in 0..M {
+            for p in 0..P {
+                for n in 0..N {
+                    array[m][p] += self.array[m][n] * rhs.array[n][p];
+                }
+            }
+        }
+        Matrix { array }
+    }
+}
+
+// Orphan rule means we have to explicitly create LHS scalar implementation
+// for each type we want to support.
+macro_rules! impl_scalar_lhs {
+    ($($t:ty),*) => {
+        $(
+            impl<const M: usize, const N: usize> Mul<Matrix<$t, M, N>> for $t {
+                type Output = Matrix<$t, M, N>;
+
+                fn mul(self, rhs: Matrix<$t, M, N>) -> Matrix<$t, M, N> {
+                    rhs * self
+                }
+            }
+        )*
+    };
+}
+
+impl_scalar_lhs!(i8, i16, i32, i64, f32, f64, u8, u16, u32, u64);
+
+impl<T: Numeric, const M: usize, const N: usize> fmt::Display for Matrix<T, M, N> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Matrix {}x{}\n", M, N)?;
+        for m in 0..M {
+            if m > 0 {
+                write!(f, "\n")?;
+            }
+            for n in 0..N {
+                if m == 0 && n == 0 {
+                    write!(f, "⌈\t")?;
+                } else if m == M - 1 && n == 0 {
+                    write!(f, "⌊\t")?;
+                } else if n == 0 {
+                    write!(f, "|\t")?;
+                }
+                write!(f, "{}\t", self.array[m][n])?;
+                if m == 0 && n == N - 1 {
+                    write!(f, "⌉")?;
+                } else if m == M - 1 && n == N - 1 {
+                    write!(f, "⌋")?;
+                } else if n == N - 1 {
+                    write!(f, "|")?;
+                }
+            }
+        }
+        write!(f, "")
+    }
+}
---
+++ tests/tests.rs
@@ -0,0 +1,135 @@
+use rust_matrices::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn constructors() {
+        let array: [[i32; 1]; 3] = [
+            [i32::default()],
+            [i32::default()],
+            [i32::default()]
+        ];
+        let matrix = Matrix::from_array(array);
+        let m: Matrix<i32, 3, 1> = Matrix::new();
+        assert_eq!(m, matrix);
+    }
+
+    #[test]
+    fn add() {
+
+        let m1 = Matrix::from_array([
+            [1],
+            [2],
+            [3]
+        ]);
+        let m2 = Matrix::from_array([
+            [3],
+            [4],
+            [5]
+        ]);
+        let m3 = Matrix::from_array([
+            [4],
+            [6],
+            [8]
+        ]);
+        assert_eq!(m1 + m2, m3);
+        assert_eq!(m2 + m1, m3); // Associative
+    }
+
+    #[test]
+    fn sub() {
+
+        let m1 = Matrix::from_array([
+            [3],
+            [4],
+            [5]
+        ]);
+        let m2 = Matrix::from_array([
+            [1],
+            [2],
+            [3]
+        ]);
+        let m3 = Matrix::from_array([
+            [2],
+            [2],
+            [2]
+        ]);
+        assert_eq!(m1 - m2, m3);
+        assert_ne!(m2 - m1, m3); // Not associative
+    }
+
+
+    #[test]
+    fn scalar() {
+
+        let m = Matrix::from_array([
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+        ]);
+        let m2 = Matrix::from_array([
+            [3,6,9],
+            [12,15,18],
+            [21,24,27]
+        ]);
+        assert_eq!(m.clone() * 3, m2); // Right hand side impl
+        assert_eq!(3 * m, m2); // Left hand side impl
+    }
+
+    #[test]
+    fn transpose() {
+
+        let m1 = Matrix::from_array([
+            [1, 2, 3],
+            [0, -6, 7],
+        ]);
+        let m2 = Matrix::from_array([
+            [1, 0],
+            [2, -6],
+            [3, 7],
+        ]);
+        assert_eq!(m1.transpose(), m2);
+        assert_eq!(m1, m2.transpose());
+        assert_eq!(m1.transpose().transpose(), m1);
+    }
+
+    #[test]
+    fn multiplication() {
+
+        let a = Matrix::from_array([
+            [2, 3, 4],
+            [1, 0, 0],
+        ]);
+        let b = Matrix::from_array([
+            [0, 1000],
+            [1, 100],
+            [0, 10],
+        ]);
+        let ab = Matrix::from_array([
+            [3, 2340],
+            [0, 1000],
+        ]);
+        assert_eq!(a * b, ab);
+        let c = Matrix::from_array([
+            [1, 2],
+            [3, 4],
+        ]);
+        let d = Matrix::from_array([
+            [0, 1],
+            [0, 0],
+        ]);
+        let e = Matrix::from_array([
+            [0, 1],
+            [0, 3],
+        ]);
+        let f = Matrix::from_array([
+            [3, 4],
+            [0, 0],
+        ]);
+        assert_eq!(c * d, e);
+        assert_ne!(d * c, e);
+        assert_eq!(d * c, f);
+        assert_ne!(c * d, f);
+    }
+}
